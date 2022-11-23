072dde16ab11302d17abf1b8c622f34949dce7bb
Daniel Gomes Assunção <106596714+007msn98@users.noreply.github.com>
2022-11-23 08:42:45 -0400

    first-commit

---
+++ scripts/theme.js
@@ -0,0 +1,28 @@
+function changeTheme() {
+  const changeThemeBtn = document.getElementById("change-theme-btn");
+  const html = document.querySelector("html");
+
+  const darkModePref = localStorage.getItem("darkmode");
+
+  if (darkModePref) {
+    html.classList.add("dark-mode");
+    changeThemeBtn.classList.toggle("light-icon");
+    changeThemeBtn.classList.toggle("dark-icon");
+  }
+
+  changeThemeBtn.addEventListener("click", () => {
+    const darkModePref = localStorage.getItem("darkmode");
+    if (!darkModePref) {
+      localStorage.setItem("darkmode", true);
+    }
+    if (darkModePref) {
+      localStorage.removeItem("darkmode");
+    }
+
+    html.classList.toggle("dark-mode");
+    changeThemeBtn.classList.toggle("light-icon");
+    changeThemeBtn.classList.toggle("dark-icon");
+  });
+}
+
+changeTheme();
